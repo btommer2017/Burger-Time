ee27572d9dc543ecd59e27ab2a7f1a9cc193fa6a
btommer2017 <btommer@gmail.com>
2018-05-18 13:42:32 -0500

    added mysql connection and schema/seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name) VALUES ('In-N-Out Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Krusty Burger',true);
